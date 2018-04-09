b518c07669d94620737fc0cb1cd2e4ea5d2735f8
Haowen <haowen737@gmail.com>
2018-04-09 14:23:56 +0800

    fix: dev config

--- webApp/config/index.js
+++ webApp/config/index.js
@@ -13,7 +13,7 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
+    host: '0.0.0.0', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
@@ -33,7 +33,9 @@ module.exports = {
      */
 
     // https://webpack.js.org/configuration/devtool/#development
-    devtool: 'eval-source-map',
+    // devtool: 'eval-source-map',
+    devtool: 'inline-source-map',
+    
 
     // If you have problems debugging vue-files in devtools,
     // set this to false - it *may* help
